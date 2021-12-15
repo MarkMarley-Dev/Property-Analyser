befbe43205a1fb7d91e725817e7a2e14376be046
MarkMarley-Dev <markmarley19911@gmail.com>
2021-12-15 11:43:18 +0000

    firebase

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -18,35 +18,65 @@ import AreaStats from "./Components/Search/AreaStats/AreaStats";
 import AgentSearch from "./Components/Search/AreaStats/AgentSearch/AgentSearch";
 import CrimeSearch from "./Components/Search/AreaStats/Crime/Crime";
 import DemographicsSearch from "./Components/Search/AreaStats/Demographics/DemographicsSearch";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
+import Reset from "./Components/Reset/Reset";
+import Account from "./components/Account/Account";
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={Home}></Route>
-        <Route path="/search" component={Search}></Route>
-        <Route path="/price" component={Price}></Route>
-        <Route path="/avg" component={AvgPrice}></Route>
-        <Route path="/pricepersqft" component={PricePerSqft}></Route>
-        <Route path="/soldAvg" component={SoldAvgPrice}></Route>
-        <Route path="/soldpricepersqft" component={SoldPerSqFt}></Route>
-        <Route path="/demandsale" component={DemandSale}></Route>
-        <Route path="/growth" component={GrowthSearch}></Route>
-        <Route path="/stampduty" component={StampDuty}></Route>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home}></Route>
+          <Route path="/search" component={Search}></Route>
+          <Route path="/price" component={Price}></Route>
+          <Route path="/avg" component={AvgPrice}></Route>
+          <Route path="/pricepersqft" component={PricePerSqft}></Route>
+          <Route path="/soldAvg" component={SoldAvgPrice}></Route>
+          <Route path="/soldpricepersqft" component={SoldPerSqFt}></Route>
+          <Route path="/demandsale" component={DemandSale}></Route>
+          <Route path="/growth" component={GrowthSearch}></Route>
+          <Route path="/stampduty" component={StampDuty}></Route>
 
-        <Route path="/rentsyields" component={RentsYields}></Route>
-        <Route path="/rentsavg" component={RentsAvg}></Route>
-        <Route path="/rentshmo" component={RentsHmo}></Route>
-        <Route path="/yield" component={Yield}></Route>
-        <Route path="/demandrental" component={DemandRental}></Route>
+          <Route path="/rentsyields" component={RentsYields}></Route>
+          <Route path="/rentsavg" component={RentsAvg}></Route>
+          <Route path="/rentshmo" component={RentsHmo}></Route>
+          <Route path="/yield" component={Yield}></Route>
+          <Route path="/demandrental" component={DemandRental}></Route>
 
-        <Route path="/areaStats" component={AreaStats}></Route>
-        <Route path="/agentSearch" component={AgentSearch}></Route>
-        <Route path="/crime" component={CrimeSearch}></Route>
-        <Route path="/demographics" component={DemographicsSearch}></Route>
-      </Switch>
-    </BrowserRouter>
+          <Route path="/areaStats" component={AreaStats}></Route>
+          <Route path="/agentSearch" component={AgentSearch}></Route>
+          <Route path="/crime" component={CrimeSearch}></Route>
+          <Route path="/demographics" component={DemographicsSearch}></Route>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/reset" component={Reset} />
+          <Route exact path="/account" component={Account} />
+        </Switch>
+      </BrowserRouter>
+      <CookieConsent
+        location="bottom"
+        buttonText="No problem!"
+        style={{
+          background: "#f6416c",
+          paddingBottom: "0.5rem",
+          maxWidth: "100%",
+        }}
+        buttonStyle={{
+          color: "#ffde7d",
+          color: "#000",
+          fontSize: "13px",
+          borderRadius: "5px",
+          fontFamily: "Roboto Mono,monospace",
+        }}
+        expires={150}
+      >
+        This website uses cookies to enhance the user experience.{" "}
+        <span style={{ fontSize: "10px" }}>We use pizzas to!</span>
+      </CookieConsent>
+    </>
   );
 }
 
---
+++ client/my-app/src/Components/Login/Login.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import {
+  auth,
+  signInWithEmailAndPassword,
+  signInWithGoogle,
+} from "../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import "./Login.scss";
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const history = useHistory();
+  useEffect(() => {
+    if (loading) {
+      // maybe trigger a loading screen
+      return;
+    }
+    if (user) history.replace("/account");
+  }, [user, loading]);
+  return (
+    <div className="content">
+      <h1 className="content__title">login</h1>
+      <div className="login">
+        <div className="login__container">
+          <input
+            type="text"
+            className="login__textBox"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="E-mail Address"
+          />
+          <input
+            type="password"
+            className="login__textBox"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+          />
+          <button
+            className="button"
+            onClick={() => signInWithEmailAndPassword(email, password)}
+          >
+            login
+          </button>
+          <button className="button button__google" onClick={signInWithGoogle}>
+            login with Google
+          </button>
+          <div>
+            <Link to="/reset">Forgot Password</Link>
+          </div>
+          <div>
+            Don't have an account? <Link to="/register">Register</Link> now.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Login;
